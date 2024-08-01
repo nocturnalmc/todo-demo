1a4b7c3a519c65f0ec60727be994258075f339aa
Dvachini <izzuddinazman5@gmail.com>
2024-08-01 12:12:33 +0800

    feat: add model for todo

--- references/models/Todo.js
+++ references/models/Todo.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const TodoSchema = new mongoose.Schema(
   {
     jenisTodo: { type: String, default: '' },
-    contentTodo: { type: String, required: true },
-    done: { type: Boolean, required: true, default: false },
+    tajukTodo: { type: String, required: true },
+    isiTodo: { type: String, required: true },
+    doneTodo: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
