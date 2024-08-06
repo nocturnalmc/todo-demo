4498106f839419d9935f338fb1456af1d188741f
Dvachini <izzuddinazman5@gmail.com>
2024-08-06 16:26:10 +0800

    fix: model without required

--- references/models/Todo.js
+++ references/models/Todo.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const TodoSchema = new mongoose.Schema(
   {
     jenisTodo: { type: String, default: '' },
-    tajukTodo: { type: String, required: true },
-    isiTodo: { type: String, required: true },
-    doneTodo: { type: Boolean, required: true, default: false },
+    tajukTodo: { type: String, default: '' },
+    isiTodo: { type: String, default: '' },
+    doneTodo: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
