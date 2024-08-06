92de2282b9bbbee085e86bf474c4aef5b74835de
Dvachini <izzuddinazman5@gmail.com>
2024-08-06 16:26:40 +0800

    fix: CRUD To-Do App List

--- references/client/src/components/ModalTodo.jsx
+++ references/client/src/components/ModalTodo.jsx
@@ -1,34 +1,132 @@
-export default function ModalTodo({ setOpenModalTodo }) {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function ModalTodo({
+  setOpenModalTodo,
+  idTodo,
+  setIdTodo,
+  reload,
+  setReload,
+}) {
+  const [jenisTodo, setJenisTodo] = useState('');
+  const [tajukTodo, setTajukTodo] = useState('');
+  const [isiTodo, setIsiTodo] = useState('');
+  const [doneTodo, setDoneTodo] = useState(false);
+
+  useEffect(() => {
+    if (idTodo) {
+      const fetchSingleTodo = async () => {
+        try {
+          const { data } = await axios.get(`/api/todo/${idTodo}`);
+          console.log(data.singleTodo);
+          setJenisTodo(data.singleTodo.jenisTodo);
+          setTajukTodo(data.singleTodo.tajukTodo);
+          setIsiTodo(data.singleTodo.isiTodo);
+          setDoneTodo(data.singleTodo.doneTodo);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchSingleTodo();
+    }
+  }, [idTodo]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!idTodo) {
+      try {
+        const response = await axios.post('/api/todo', {
+          jenisTodo,
+          tajukTodo,
+          isiTodo,
+          doneTodo,
+        });
+
+        console.log(response.data);
+        setOpenModalTodo(false);
+        setReload(!reload);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    if (idTodo) {
+      try {
+        const response = await axios.patch(`/api/todo/${idTodo}`, {
+          jenisTodo,
+          tajukTodo,
+          isiTodo,
+          doneTodo,
+        });
+
+        console.log(response.data);
+        setIdTodo('');
+        setOpenModalTodo(false);
+        setReload(!reload);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center'>
-      <form className='w-96 bg-white rounded-md p-4'>
+      <form onSubmit={handleSubmit} className='w-96 bg-white rounded-md p-4'>
         <h2 className='font-medium text-lg'>Tambah Todo</h2>
         <select
           className='w-full border border-gray-300 rounded-md p-2 mt-2'
           placeholder='Jenis'
+          required
+          name='jenis-todo'
+          id='jenis-todo'
+          value={jenisTodo}
+          onChange={(e) => setJenisTodo(e.target.value)}
         >
-          <option></option>
-          <option>Kerja</option>
-          <option>Hiburan</option>
-          <option>Keluarga</option>
+          <option value=''></option>
+          <option value='kerja'>Kerja</option>
+          <option value='hiburan'>Hiburan</option>
+          <option value='keluarga'>Keluarga</option>
         </select>
         <input
           type='text'
           className='w-full border border-gray-300 rounded-md p-2 mt-2'
           placeholder='Tajuk'
+          required
+          name='tajuk-todo'
+          id='tajuk-todo'
+          value={tajukTodo}
+          onChange={(e) => setTajukTodo(e.target.value)}
         />
         <textarea
           className='w-full border border-gray-300 rounded-md p-2 mt-2'
           placeholder='Isi'
+          required
+          name='isi-todo'
+          id='isi-todo'
+          value={isiTodo}
+          onChange={(e) => setIsiTodo(e.target.value)}
         />
-        <label className='w-full p-2 mt-2 flex items-center justify-center space-x-2'>
-          <input type='checkbox' />
+        <label
+          htmlFor='done-todo'
+          className='w-full p-2 mt-2 flex items-center justify-center space-x-2'
+        >
+          <input
+            type='checkbox'
+            name='done-todo'
+            id='done-todo'
+            checked={doneTodo}
+            onChange={(e) => setDoneTodo(!doneTodo)}
+          />
           <span>Selesai</span>
         </label>
         <div className='flex flex-row justify-end mt-2 space-x-2'>
           <button
             type='button'
-            onClick={() => setOpenModalTodo(false)}
+            onClick={() => {
+              setIdTodo('');
+              setOpenModalTodo(false);
+            }}
             className='bg-red-500 text-white rounded-md p-2 w-24'
           >
             Batal
--- references/client/src/components/TodoMain.jsx
+++ references/client/src/components/TodoMain.jsx
@@ -1,26 +1,72 @@
-import { useState } from 'react';
-import { FaPlus, FaRegEdit, FaRegTrashAlt } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { FaPlus, FaRegEdit, FaRegTrashAlt, FaRedoAlt } from 'react-icons/fa';
 import ModalTodo from './ModalTodo';
+import axios from 'axios';
 
 export default function TodoMain() {
+  const [semuaTodo, setSemuaTodo] = useState([]);
+  const [idTodo, setIdTodo] = useState('');
+
   const [openModalTodo, setOpenModalTodo] = useState(false);
+
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    const fetchSemuaTodo = async () => {
+      try {
+        const { data } = await axios.get('/api/todo');
+        console.log(data.allTodo);
+        setSemuaTodo(data.allTodo);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSemuaTodo();
+  }, [reload]);
+
+  const deleteTodo = async (id) => {
+    try {
+      const response = await axios.patch(`/api/todo/delete/${id}`);
+      console.log(response.data);
+      setReload(!reload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className='flex flex-col items-center'>
       <h1 className='text-lg font-medium w-full flex flex-row justify-between items-center'>
         <span>todo</span>
-        <span className='text-orange-500'>
+        <span className='text-orange-500 flex space-x-2'>
+          <FaRedoAlt onClick={() => setReload(!reload)} />
           <FaPlus onClick={() => setOpenModalTodo(true)} />
         </span>
       </h1>
-      <div className='w-96 rounded-md bg-orange-100 p-4'>
-        <h2 className='font-medium text-lg'>Tajuk</h2>
-        <p className='whitespace-pre-wrap'>Isi</p>
-        <span className='flex justify-end space-x-2 text-lg'>
-          <FaRegEdit />
-          <FaRegTrashAlt />
-        </span>
-      </div>
-      {openModalTodo && <ModalTodo setOpenModalTodo={setOpenModalTodo} />}
+      {semuaTodo.map((todo, index) => (
+        <div className='w-96 rounded-md bg-orange-100 p-4'>
+          <h2 className='font-medium text-lg'>{todo.tajukTodo}</h2>
+          <p className='whitespace-pre-wrap'>{todo.isiTodo}</p>
+          <span className='flex justify-end space-x-2 text-lg'>
+            <FaRegEdit
+              onClick={() => {
+                setIdTodo(todo._id);
+                setOpenModalTodo(true);
+              }}
+            />
+            <FaRegTrashAlt onClick={() => deleteTodo(todo._id)} />
+          </span>
+        </div>
+      ))}
+      {openModalTodo && (
+        <ModalTodo
+          setOpenModalTodo={setOpenModalTodo}
+          idTodo={idTodo}
+          setIdTodo={setIdTodo}
+          reload={reload}
+          setReload={setReload}
+        />
+      )}
     </div>
   );
 }
